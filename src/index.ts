37d586f9ba8dd7026655d277498bd715acb7d9d6
Raymond <raymond.lo@yahoo.com>
2022-03-05 05:55:28 -0500

    init mikro-orm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: "cleddit",
+    debug: __prod__,
+    type: "postgresql",
+  });
+};
+
+main();
