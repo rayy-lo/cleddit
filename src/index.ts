b04b63d7bc182300901c2379f2225ff17e12b7e5
Raymond <raymond.lo@yahoo.com>
2022-03-11 02:02:49 -0500

    remove hello resolver

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import "reflect-metadata";
+import express from "express";
 import { createConnection } from "typeorm";
 import { __prod__ } from "./constants";
-import { Post } from "./entity/Post";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 createConnection()
   .then(async (connection) => {
-    console.log("successful connection");
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [PostResolver],
+        validate: false,
+      }),
+    });
+
+    await apolloServer.start();
+
+    apolloServer.applyMiddleware({ app });
+
+    app.listen(3100, () => {
+      console.log("server started on port 3100");
+    });
   })
   .catch((error) => console.log("Error: ", error));
