6608ef8da8cdaf84e9b25d82ea0cffbcee98874d
Raymond <raymond.lo@yahoo.com>
2022-03-16 00:10:12 -0400

    use redis session

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,49 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { createClient } from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 createConnection()
   .then(async (connection) => {
     const app = express();
+
+    const RedisStore = connectRedis(session);
+    const redisClient = createClient();
+
+    app.set("trust proxy", process.env.NODE_ENV !== "production");
+
+    app.use(
+      session({
+        name: "qid",
+        store: new RedisStore({ client: redisClient }),
+        saveUninitialized: false,
+        secret: "keyboard cat",
+        resave: false,
+        cookie: {
+          maxAge: 1000 * 60 * 60 * 24,
+          httpOnly: true,
+          sameSite: "none",
+          secure: true,
+        },
+      })
+    );
 
     const apolloServer = new ApolloServer({
       schema: await buildSchema({
         resolvers: [PostResolver, UserResolver],
         validate: false,
       }),
+      context: ({ req, res }) => ({ req, res }),
     });
 
     await apolloServer.start();
 
-    apolloServer.applyMiddleware({ app });
+    apolloServer.applyMiddleware({
+      app,
+      cors: { credentials: true, origin: "https://studio.apollographql.com" },
+    });
 
     app.listen(3100, () => {
       console.log("server started on port 3100");
