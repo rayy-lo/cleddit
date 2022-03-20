00d9c210290e69a489eec936f5688f1f1225b747
Raymond <raymond.lo@yahoo.com>
2022-03-20 14:02:17 -0400

    Add apollo provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: "http://localhost:3100/graphql",
+    }),
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 };
 
