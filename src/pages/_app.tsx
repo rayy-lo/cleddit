0c14e12f5d59e7107097ad8dd1288e57218c554a
Raymond <raymond.lo@yahoo.com>
2022-03-23 02:56:28 -0400

    add client type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,13 @@ import {
   ApolloProvider,
   HttpLink,
   InMemoryCache,
+  NormalizedCacheObject,
 } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const client = new ApolloClient({
+  const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     link: new HttpLink({
       uri: "http://localhost:3100/graphql",
     }),
